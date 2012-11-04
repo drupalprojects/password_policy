f4e919bb0a14376e5e494dc746d43f949da8ee28
Erik Webb <erik@erikwebb.net>
2012-11-03 21:24:33 -0400

    Fix commenting of JS.

--- password_policy.js
+++ password_policy.js
@@ -4,17 +4,16 @@
  */
 Drupal.behaviors.passwordOverride = {
   attach: function (context, settings) {
-    // set a default for our pw_status
+    // Set a default for our pw_status.
     pw_status = {
       strength: 0,
       message: '',
       indicatorText: '',
     }
 
-    //we take over the keyup function on password and instead make a call to 
-    //the server to evaluate the password.
-    //when we get the status back we update it.  Then we call focus to all 
-    //the normal drupal password update
+    // We take over the keyup function on password and instead make a call to 
+    // the server to evaluate the password. When we get the status back we
+    // update it.  Then we call focus to all the normal drupal password update.
     $('input.password-field', context).once('passworda', function () {
       passwordInput = $(this);
       passwordCheck = function (e) {
@@ -29,8 +28,8 @@ Drupal.behaviors.passwordOverride = {
       };
       passwordInput.keyup(passwordCheck);
     });
-    //we are overriding the normal evaluatePasseordStrength and instead 
-    //are just returning the current status
+    // We are overriding the normal evaluatePasswordStrength and instead are
+    // just returning the current status.
     Drupal.evaluatePasswordStrength = function (password, translate) {
       return pw_status;
     };
