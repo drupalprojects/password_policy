3cd3413798406d4f0490285e6740100c4794ebce
Adam Bergstein <nerdstein@gmail.com>
2015-04-13 20:26:29 -0400

    Fixing permissions page to match policies, not constraints

--- src/PasswordConstraintPermissions.php
+++ src/PasswordConstraintPermissions.php
@@ -8,45 +8,17 @@ namespace Drupal\password_policy;
 class PasswordConstraintPermissions {
   public function permissions() {
     $permissions = [];
-    /**
-     * PERMISSIONS DEFINED FROM PASSWORD RESET
-     */
-    $policies = db_select('password_policy_reset', 'ppr')
-      ->fields('ppr', array())
+
+    $policies = db_select('password_policies', 'p')
+      ->fields('p', array())
       ->execute()
       ->fetchAll();
+
     foreach ($policies as $policy) {
-      $permission = 'enforce password_reset.' . $policy->pid . ' constraint';
+      $permission = 'enforce password policy ' . $policy->pid;
       $permissions[$permission] = [
-        'title' => 'Apply password reset policy: Reset after ' . $policy->number_of_days . ' days',
-        'description' => 'All passwords will be automatically forced to reset after the specified number of days',
+        'title' => 'Enforce policy: '.$policy->policy_title,
       ];
-    }
-
-    /**
-     * PERMISSIONS DEFINED FROM PLUGINS
-     */
-    //load plugins
-    $plugin_manager = \Drupal::service('plugin.manager.password_policy.password_constraint');
-    //dpm($plugin_manager);
-    $plugins = $plugin_manager->getDefinitions();
-
-    //create a perm per plugin
-    foreach ($plugins as $plugin) {
-      //dpm($plugin);
-      $plugin_instance = \Drupal::service('plugin.manager.password_policy.password_constraint')
-        ->createInstance($plugin['id']);
-
-      $policies = $plugin_instance->getPolicies();
-
-      //dpm($plugin_instance);
-      foreach ($policies as $policy_key => $policy_text) {
-        $permission = 'enforce ' . $plugin['id'] . '.' . $policy_key . ' constraint';
-        $permissions[$permission] = [
-          'title' => 'Apply password policy: ' . $policy_text,
-          'description' => $plugin['description'],
-        ];
-      }
     }
     return $permissions;
   }
