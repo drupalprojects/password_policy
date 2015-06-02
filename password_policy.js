07cfc2e63975f21602020f2689f93657d06682be
AohRveTPV <AohRveTPV@2760115.no-reply.drupal.org>
2015-06-01 20:50:08 -0500

    Remove unnecessary negative logic in condition.
    The if/else conditions can be swapped to avoid the negative logic. This
    makes the code a tad easier to read.

--- password_policy.js
+++ password_policy.js
@@ -74,111 +74,111 @@ Drupal.behaviors.passwordPolicyConstraintSettingsSummary = {
   attach: function (context) {
     $('fieldset#edit-alpha-case-fieldset', context).drupalSetSummary(function (context) {
       alpha_case = $('input[name="alpha_case"]', context).is(':checked');
-      if (!alpha_case) {
-        return Drupal.t('Not enforced');
+      if (alpha_case) {
+        return Drupal.t('Upper and lower case letters required');
       }
       else {
-        return Drupal.t('Upper and lower case letters required');
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-alpha-count-fieldset', context).drupalSetSummary(function (context) {
       alpha_count = $('input[name="alpha_count"]', context).val();
-      if (!alpha_count) {
-        return Drupal.t('Not enforced');
+      if (alpha_count) {
+        return Drupal.t('At least @count letters', {'@count': alpha_count});
       }
       else {
-        return Drupal.t('At least @count letters', {'@count': alpha_count});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-delay-fieldset', context).drupalSetSummary(function (context) {
       delay = $('input[name="delay"]', context).val();
-      if (!delay) {
-        return Drupal.t('Not enforced');
+      if (delay) {
+        return Drupal.t('@delay between changes', {'@delay': delay});
       }
       else {
-        return Drupal.t('@delay between changes', {'@delay': delay});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-blacklist-fieldset', context).drupalSetSummary(function (context) {
       blacklist = $('textarea[name="blacklist"]', context).val();
-      if (!blacklist) {
-        return Drupal.t('Not enforced');
-      }
-      else {
+      if (blacklist) {
         return Drupal.t('Must not be in blacklist');
+      }
+	  else {
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-char-count-fieldset', context).drupalSetSummary(function (context) {
       char_count = $('input[name="char_count"]', context).val();
-      if (!char_count) {
-        return Drupal.t('Not enforced');
+      if (char_count) {
+        return Drupal.t('At least @count characters', {'@count': char_count});
       }
       else {
-        return Drupal.t('At least @count characters', {'@count': char_count});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-consecutive-char-count-fieldset', context).drupalSetSummary(function (context) {
       consecutive_char_count = $('input[name="consecutive_char_count"]', context).val();
-      if (!consecutive_char_count) {
-        return Drupal.t('Not enforced');
+      if (consecutive_char_count) {
+        return Drupal.t('No more than @count consecutive characters', {'@count': consecutive_char_count});
       }
       else {
-        return Drupal.t('No more than @count consecutive characters', {'@count': consecutive_char_count});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-drupal-strength-fieldset', context).drupalSetSummary(function (context) {
       drupal_strength = $('input[name="drupal_strength"]', context).val();
-      if (!drupal_strength) {
-        return Drupal.t('Not enforced');
+      if (drupal_strength) {
+        return Drupal.t('At least level of @level Drupal strength', {'@level': drupal_strength});
       }
       else {
-        return Drupal.t('At least level of @level Drupal strength', {'@level': drupal_strength});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-int-count-fieldset', context).drupalSetSummary(function (context) {
       int_count = $('input[name="int_count"]', context).val();
-      if (!int_count) {
-        return Drupal.t('Not enforced');
+      if (int_count) {
+        return Drupal.t('At least @count integers', {'@count': int_count});
       }
       else {
-        return Drupal.t('At least @count integers', {'@count': int_count});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-past-passwords-fieldset', context).drupalSetSummary(function (context) {
       past_passwords = $('input[name="past_passwords"]', context).val();
-      if (!past_passwords) {
-        return Drupal.t('Not enforced');
+      if (past_passwords) {
+        return Drupal.t('Must not match previous @count passwords', {'@count': past_passwords});
       }
       else {
-        return Drupal.t('Must not match previous @count passwords', {'@count': past_passwords});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-symbol-count-fieldset', context).drupalSetSummary(function (context) {
       symbol_count = $('input[name="symbol_count"]', context).val();
-      if (!symbol_count) {
-        return Drupal.t('Not enforced');
+      if (symbol_count) {
+        return Drupal.t('At least @count symbols', {'@count': symbol_count}) + '<br/>' + Drupal.t('Symbol characters: @symbols', {'@symbols': $('input[name="symbol_count_symbols"]', context).val()});
       }
       else {
-        return Drupal.t('At least @count symbols', {'@count': symbol_count}) + '<br/>' + Drupal.t('Symbol characters: @symbols', {'@symbols': $('input[name="symbol_count_symbols"]', context).val()});
+        return Drupal.t('Not enforced');
       }
     });
 
     $('fieldset#edit-username-fieldset', context).drupalSetSummary(function (context) {
       var username = $('input[name="username"]:checked', context);
-      if (!username.val()) {
-        return Drupal.t('Not enforced');
+      if (username.val()) {
+        return username.next('label').text();
       }
       else {
-        return username.next('label').text();
+        return Drupal.t('Not enforced');
       }
     });
   }
